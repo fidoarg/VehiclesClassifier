7b76fd8e6bfb13de28377def2382a688786818f1
Fido Aragon <fidoarg@gmail.com>
2023-01-23 17:30:39 -0300

    Include scripts for data augmentation

--- project/utils/data_aug.py
+++ project/utils/data_aug.py
@@ -1,5 +1,7 @@
 
-def create_data_aug_layer(data_aug_layer):
+from keras import layers, Sequential
+
+def create_data_aug_layer(data_aug_layer: dict):
     """
     Use this function to parse the data augmentation methods for the
     experiment and create the corresponding layers.
@@ -24,15 +26,24 @@ def create_data_aug_layer(data_aug_layer):
         Sequential model having the data augmentation layers inside.
     """
     # Parse config and create layers
-    # You can use as a guide on how to pass config parameters to keras
-    # looking at the code in `scripts/train.py`
-    # TODO
     # Append the data augmentation layers on this list
     data_aug_layers = []
+    for key, data_aug_parameters in data_aug_layer.items():
+        if key == "random_flip":
+            data_aug_layers.append(
+                layers.RandomFlip(**data_aug_parameters)
+            )
+        if key == "random_rotation":
+            data_aug_layers.append(
+                layers.RandomRotation(**data_aug_parameters)
+            )
+        if key == "random_zoom":
+            data_aug_layers.append(
+                layers.RandomZoom(**data_aug_parameters)
+            )
 
     # Return a keras.Sequential model having the the new layers created
     # Assign to `data_augmentation` variable
-    # TODO
-    data_augmentation = None
+    data_augmentation = Sequential(data_aug_layers) if len(data_aug_layers) else None
 
     return data_augmentation
